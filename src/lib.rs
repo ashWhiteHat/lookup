3448cd86e090b6a499cb79ae41725ab46e22c7d0
ashWhiteHat <phantomofrotten@gmail.com>
2023-10-05 13:51:29 +0900

    bit decomposition check

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+mod plookup;
 mod range_proof;
--- src/range_proof.rs
+++ src/range_proof.rs
@@ -1,10 +1,33 @@
+use zkstd::common::PrimeField;
+
+fn binary_check<F: PrimeField>(value: F) -> bool {
+    let binary = value
+        .to_bits()
+        .iter()
+        .map(|bit| *bit as i8)
+        .collect::<Vec<_>>();
+    let binary_prime = binary.iter().map(|bit| *bit - 1).collect::<Vec<_>>();
+    binary
+        .iter()
+        .zip(binary_prime.iter())
+        .all(|(bit, bit_prime)| bit * bit_prime == 0)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::binary_check;
+
     use bls_12_381::Fr as Scalar;
     use ec_pairing::TatePairing;
     use poly_commit::KzgParams;
     use rand::rngs::OsRng;
     use zkstd::common::Group;
+
+    #[test]
+    fn binary_check_test() {
+        let z = Scalar::random(OsRng);
+        assert!(binary_check(z))
+    }
 
     #[test]
     fn range_proof_test() {
