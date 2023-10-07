4e2b6142e7c05370a933149c9f4a180bd5059a5a
ashWhiteHat <phantomofrotten@gmail.com>
2023-10-07 15:50:13 +0900

    polynomial ops test

--- src/inner_product.rs
+++ src/inner_product.rs
@@ -1,7 +1,8 @@
+use rand::{rngs::OsRng, seq::IteratorRandom};
 use zkstd::common::FftField;
 
 pub(crate) struct Polynomial<F: FftField> {
-    coeffs: Vec<F>,
+    pub(crate) coeffs: Vec<F>,
 }
 
 impl<F: FftField> Polynomial<F> {
@@ -10,11 +11,80 @@ impl<F: FftField> Polynomial<F> {
     }
 
     // log(n) inner product
-    pub(crate) fn inner_product(&self, rhs: Self) -> F {
+    pub(crate) fn inner_product(&self, rhs: &Self) -> F {
         assert_eq!(self.coeffs.len(), rhs.coeffs.len());
         self.coeffs
             .iter()
             .zip(rhs.coeffs.iter())
             .fold(F::zero(), |sum, (a, b)| sum + *a * *b)
+    }
+
+    pub(crate) fn random(k: usize) -> Self {
+        let n = 1 << k;
+        let coeffs = (0..n).map(|_| F::random(OsRng)).collect();
+        Self { coeffs }
+    }
+
+    pub(crate) fn half(self) -> (Self, Self) {
+        let n = self.coeffs.len();
+        let half = n / 2;
+        let (lo, hi) = self.coeffs.split_at(half);
+        (
+            Self {
+                coeffs: lo.to_vec(),
+            },
+            Self {
+                coeffs: hi.to_vec(),
+            },
+        )
+    }
+
+    pub(crate) fn scalar(self, scalar: F) -> Self {
+        let coeffs = self.coeffs.iter().map(|coeff| *coeff * scalar).collect();
+        Self { coeffs }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use bls_12_381::Fr as Scalar;
+    use zkstd::common::{Group, PrimeField};
+
+    #[test]
+    fn inner_product_proof_test() {
+        // setup
+        let k = 8;
+        let x = Scalar::random(OsRng);
+        let x_inv = x.invert().unwrap();
+        let xx = x.square();
+        let xx_inv = x_inv.square();
+        let a_poly = Polynomial::<Scalar>::random(k);
+        let b_poly = Polynomial::<Scalar>::random(k);
+
+        // compress
+        let (alo, ahi) = a_poly.half();
+        let (blo, bhi) = b_poly.half();
+        let c = alo.inner_product(&blo) + ahi.inner_product(&bhi);
+        let l = alo.inner_product(&bhi);
+        let r = ahi.inner_product(&blo);
+        let xx_l = xx * l;
+        let xx_inv_r = xx_inv * r;
+        let c_prime = c + xx_l + xx_inv_r;
+    }
+
+    #[test]
+    fn polynomial_ops_test() {
+        let k = 8;
+        let a_poly = Polynomial::<Scalar>::random(k);
+        let b_poly = Polynomial::<Scalar>::random(k);
+        let naive_product = a_poly.inner_product(&b_poly);
+
+        let (alo, ahi) = a_poly.half();
+        let (blo, bhi) = b_poly.half();
+        let half_product = alo.inner_product(&blo) + ahi.inner_product(&bhi);
+
+        assert_eq!(naive_product, half_product)
     }
 }
--- src/kate_commitment.rs
+++ src/kate_commitment.rs
@@ -1,3 +1,5 @@
+use crate::inner_product::Polynomial;
+
 use zkstd::behave::{CurveGroup, FftField, Group, Pairing};
 use zkstd::common::RngCore;
 
@@ -17,5 +19,16 @@ impl<P: Pairing> KateCommitment<P> {
             .collect::<Vec<_>>();
         let h = (P::G2Affine::ADDITIVE_GENERATOR * t).into();
         Self { g, h }
+    }
+
+    pub(crate) fn commit(&self, polynomial: Polynomial<P::ScalarField>) -> P::G1Affine {
+        polynomial
+            .coeffs
+            .iter()
+            .zip(self.g.iter())
+            .fold(P::G1Projective::ADDITIVE_IDENTITY, |sum, (scalar, base)| {
+                sum + *base * *scalar
+            })
+            .into()
     }
 }
