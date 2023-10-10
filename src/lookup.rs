f1e79dc50ecf683ca8de75fedb120cd8e2bb155c
ashWhiteHat <phantomofrotten@gmail.com>
2023-10-10 14:23:23 +0900

    naive check with table

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,6 +1,7 @@
 use zkstd::common::PrimeField;
 
 pub(crate) struct XORTable<F: PrimeField> {
+    n: usize,
     a: Vec<F>,
     b: Vec<F>,
     c: Vec<F>,
@@ -19,7 +20,17 @@ impl<F: PrimeField> XORTable<F> {
                 c.push(F::from(k as u64));
             }
         }
-        Self { a, b, c }
+        Self { n, a, b, c }
+    }
+
+    pub(crate) fn compress(&self, α: F) -> Vec<F> {
+        let αα = α.square();
+        self.a
+            .iter()
+            .zip(self.b.iter())
+            .zip(self.c.iter())
+            .map(|((t1, t2), t3)| *t1 + α * t2 + αα * t3)
+            .collect()
     }
 }
 
@@ -27,9 +38,16 @@ impl<F: PrimeField> XORTable<F> {
 mod tests {
     use super::XORTable;
     use bls_12_381::Fr as Scalar;
+    use rand::rngs::OsRng;
+    use rand::{thread_rng, Rng};
+    use zkstd::common::{Group, PrimeField};
+
+    fn compress_witness<F: PrimeField>(a: u64, b: u64, c: u64, α: F) -> F {
+        F::from(a) + α * F::from(b) + α.square() * F::from(c)
+    }
 
     #[test]
-    fn table_generation() {
+    fn table_generation_test() {
         let bit_length = 4;
         let length = 1 << (bit_length * 2);
         let xor_table = XORTable::<Scalar>::precompute();
@@ -44,5 +62,23 @@ mod tests {
         {
             assert_eq!(i ^ j, *k)
         }
+    }
+
+    #[test]
+    fn lookup_test() {
+        let bit_length = 4;
+        let range = 1 << bit_length;
+        let a = thread_rng().gen_range(0..range);
+        let b = thread_rng().gen_range(0..range);
+        let c = a ^ b;
+        let d = a + b;
+        let α = Scalar::random(OsRng);
+        let xor_table = XORTable::<Scalar>::precompute();
+        let ti = xor_table.compress(α);
+        let fi = compress_witness(a, b, c, α);
+        let gi = compress_witness(a, b, d, α);
+
+        assert!(ti.contains(&fi));
+        assert!(!ti.contains(&gi))
     }
 }
