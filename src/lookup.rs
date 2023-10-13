e8dfd44f19f1d03e6a9fbc87a246fed5a9f8db1c
ashWhiteHat <phantomofrotten@gmail.com>
2023-10-13 12:44:58 +0900

    compute z poly

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,19 +1,19 @@
 //! plookup protocol
 //! https://eprint.iacr.org/2020/315.pdf#page=6
 use rand::rngs::OsRng;
-use zkstd::common::PrimeField;
+use zkstd::common::{FftField, PrimeField};
 
 mod table;
 
 use table::XORTable;
 
-pub(crate) struct Lookup<F: PrimeField> {
+pub(crate) struct Lookup<F: FftField> {
     a: Vec<F>,
     b: Vec<F>,
     c: Vec<F>,
 }
 
-impl<F: PrimeField> Lookup<F> {
+impl<F: FftField> Lookup<F> {
     pub(crate) fn new(a: Vec<F>, b: Vec<F>, c: Vec<F>) -> Self {
         Self { a, b, c }
     }
@@ -21,10 +21,11 @@ impl<F: PrimeField> Lookup<F> {
     pub(crate) fn prove(&self, alpha: F, table: XORTable<F>) {
         let f = self.compress(alpha);
         let t = table.compress(alpha);
-        let mut s = [f, t].concat();
+        let mut s = [f.clone(), t.clone()].concat();
         s.sort();
         let (h1, h2) = s.split_at(s.len() / 2);
         let (β, y) = (F::random(OsRng), F::random(OsRng));
+        let z = compute_z(β, y, f, t, h1.to_vec(), h2.to_vec());
     }
 
     fn compress(&self, alpha: F) -> Vec<F> {
@@ -36,6 +37,41 @@ impl<F: PrimeField> Lookup<F> {
             .map(|((t1, t2), t3)| *t1 + alpha * t2 + alpha2 * t3)
             .collect()
     }
+}
+
+fn compute_z<F: FftField>(β: F, y: F, f: Vec<F>, t: Vec<F>, h1: Vec<F>, h2: Vec<F>) -> Vec<F> {
+    let n = f.len();
+    let one_β = F::one() + β;
+    let mut z = vec![F::one()];
+    let mut f_prev = F::one();
+    let mut s_prev = F::one();
+    for i in 2..n {
+        let fi = compute_f(i, one_β, β, y, &f, &t);
+        let gi = compute_g(i, one_β, β, y, &h1, &h2);
+        f_prev *= fi;
+        s_prev *= gi;
+        let zi = f_prev / s_prev;
+        z.push(zi)
+    }
+    z.push(F::one());
+    z
+}
+
+fn compute_f<F: FftField>(i: usize, one_β: F, β: F, y: F, f: &Vec<F>, t: &Vec<F>) -> F {
+    let left = y + f[i];
+    let right = randomly_linear_combination(one_β, β, y, t[i], t[i + 1]);
+    left * right
+}
+
+fn compute_g<F: FftField>(i: usize, one_β: F, β: F, y: F, h1: &Vec<F>, h2: &Vec<F>) -> F {
+    let left = randomly_linear_combination(one_β, β, y, h1[i], h1[i + 1]);
+    let right = randomly_linear_combination(one_β, β, y, h2[i], h2[i + 1]);
+    left * right
+}
+
+// λ(1 + β) + a_i + β a_i_1
+fn randomly_linear_combination<F: FftField>(one_β: F, β: F, y: F, a_i: F, a_i1: F) -> F {
+    y * one_β + a_i + β * a_i1
 }
 
 #[cfg(test)]
